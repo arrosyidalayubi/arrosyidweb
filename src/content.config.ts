5983358af945c17c8343ea85a530012d7f3deb4e
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-09 08:16:46 +0700

    fixing zod and slugs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { z, defineCollection } from 'astro:content';
-
+import { defineCollection } from 'astro:content';
+import z from 'astro/zod';
 const blogCollection = defineCollection({
   type: 'content',
   schema: z.object({
