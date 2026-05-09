c8367030c5ee8425a9891571cae0a679e8ead97a
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-09 09:27:56 +0700

    update main page and blog page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
 import { defineCollection } from 'astro:content';
 import z from 'astro/zod';
+import { glob } from 'astro/loaders';
+
 const blogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -12,7 +14,7 @@ const blogCollection = defineCollection({
 });
 
 const vlogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/vlog' }),
   schema: z.object({
     title: z.string(),
     location: z.string(),
