458b08617f4290a2c6404cf96ea344aa370b09e6
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-09 18:48:22 +0700

    update and fix logic for vlog, proyek, blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,26 +4,42 @@ import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     publishDate: z.date(),
-    tags: z.array(z.string()), // Wajib ada untuk fitur Related Posts
-    coverImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    coverImage: image().optional(), 
   }),
 });
 
 const vlogCollection = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/vlog' }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     location: z.string(),
     publishDate: z.date(),
-    gallery: z.array(z.string()).optional(),
+    gallery: z.array(image()).optional(), 
+  }),
+});
+
+const projectCollection = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/proyek' }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(), // Deskripsi singkat untuk kartu di halaman depan
+    client: z.string().optional(), // Nama klien atau institusi
+    role: z.string(), // Peran Anda (misal: "Fullstack Developer")
+    techStack: z.array(z.string()), // Array teknologi yang digunakan
+    publishDate: z.date(),
+    coverImage: image().optional(),
+    liveUrl: z.url().optional(), // Link ke web asli jika masih aktif
+    githubUrl: z.url().optional(),
   }),
 });
 
 export const collections = {
   'blog': blogCollection,
   'vlog': vlogCollection,
+  'proyek': projectCollection,
 };
